9d6b87517b629f0a0dd1c2c410cfdb0b5b91b73d
Leandrho <leandro.coder01@gmail.com>
2024-11-06 15:19:21 -0300

    add click handler -> addTodo

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,17 +1,38 @@
+import { useRef } from "react";
 import { TodoItemType } from "../types";
 
+let globalID = 0;
 type TodoFormProps = {
   addTodo : (todo :TodoItemType) => void;
 }
+
 export const TodoForm = ( { addTodo } :TodoFormProps) => {
+
+  const input = useRef<HTMLInputElement>(null);
   
+  const handleSubmit = () => {
+    if(input.current){
+      const ntodo :TodoItemType = {
+        id: ++globalID,
+        name: 'nuevo',
+        description: input.current.value,
+        done: false,
+      }
+      addTodo(ntodo);
+      input.current.value='';
+    }
+      
+  }
+
   return (
     <form action="#" className="h-16 flex">
       <div className="bg-white rounded-lg flex-grow flex items-center gap-6 px-4">
-        <button aria-label="submit" type="button" className="appearance-none w-7 h-7 cursor-pointer rounded-full border-2 border-[--border-check] hover:bg-gradient-to-br hover:from-[--grad-from] hover:to-[--grad-to]"/>
-        <input aria-label="Enter new task" type="text"  placeholder="Create a new todo..." className="outline-none min-h-6 text-[--text-main]" />
+        <button aria-label="submit" 
+                className="appearance-none w-7 h-7 cursor-pointer rounded-full border-2 border-[--border-check] hover:bg-gradient-to-br hover:from-[--grad-from] hover:to-[--grad-to]"
+                onClick={()=>handleSubmit()}
+                />
+        <input ref={input} aria-label="Enter new task" type="text"  placeholder="Create a new todo..." className="outline-none min-h-6 text-[--text-main]" />
       </div>
-      
     </form>
   )
 }
