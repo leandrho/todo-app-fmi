87dd4460084eccac85ed679d79cf17451d88e40b
Leandrho <leandro.coder01@gmail.com>
2024-11-06 15:21:54 -0300

    using todoform

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-
+import { useTodoApp } from "./hooks/useTodoApp"
+import { TodoForm, TodoList } from './components';
 
 function App() {
+
+  const { todoList, addTodo } = useTodoApp();
+  
   const bg = {
     backgroundImage: 'url(/images/bg-desktop-light.jpg)',
     backgroundSize: '100% 100%',
@@ -8,10 +12,13 @@ function App() {
   }
   return (
     <>
-    <div className='min-h-[300px]' style={bg}>
-      <header>
-        <h1>TODO</h1>
+    <div className='min-h-[300px] flex items-center justify-center flex-col gap-10' style={bg}>
+      <header className="w-[540px]">
+        <h1 className="text-4xl font-bold text-white tracking-[1rem]">TODO</h1>
       </header>
+      <main className="w-[540px]">
+        <TodoForm addTodo={addTodo}/>
+      </main>
     </div>
      
     </>
