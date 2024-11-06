62a544d0bed72d9313cbdce289100b0e93429c78
Leandrho <leandro.coder01@gmail.com>
2024-11-06 15:41:37 -0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,20 @@ function App() {
   
   const bg = {
     backgroundImage: 'url(/images/bg-desktop-light.jpg)',
-    backgroundSize: '100% 100%',
+    backgroundSize: '100%',
     backgroundPosition: 'top',
+    backgroundRepeat: 'no-repeat',
   }
   return (
     <>
-    <div className='min-h-[300px] flex items-center justify-center flex-col gap-10' style={bg}>
-      <header className="w-[540px]">
-        <h1 className="text-4xl font-bold text-white tracking-[1rem]">TODO</h1>
-      </header>
-      <main className="w-[540px]">
-        <TodoForm addTodo={addTodo}/>
-        <TodoList items={todoList}/>
-      </main>
+    <div className='h-screen flex items-center pt-20 flex-col gap-10' style={bg}>
+        <header className="w-[540px]">
+          <h1 className="text-4xl font-bold text-white tracking-[1rem]">TODO</h1>
+        </header>
+        <main className="w-[540px]">
+          <TodoForm addTodo={addTodo}/>
+          <TodoList items={todoList}/>
+        </main>
     </div>
      
     </>
