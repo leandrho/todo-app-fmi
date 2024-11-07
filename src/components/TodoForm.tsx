2239b8438cef81892802d25e82fd61c88b746848
Leandrho <leandro.coder01@gmail.com>
2024-11-07 11:03:16 -0300

    change w h input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -30,7 +30,7 @@ export const TodoForm = ( { addTodo } :TodoFormProps) => {
     <form action="#" className="h-16 flex">
       <div className="bg-white rounded-lg flex-grow flex items-center gap-6 px-4">
         <button aria-label="submit" 
-                className="appearance-none w-7 h-7 cursor-pointer rounded-full border-2 border-[--border-check] hover:bg-gradient-to-br hover:from-[--grad-from] hover:to-[--grad-to]"
+                className="appearance-none min-w-[28px] min-h-[28px] cursor-pointer rounded-full border-2 border-[--border-check] hover:bg-gradient-to-br hover:from-[--grad-from] hover:to-[--grad-to]"
                 onClick={()=>handleSubmit()}
                 />
         <input ref={input} aria-label="Enter new task" type="text"  placeholder="Create a new todo..." className="outline-none min-h-6 text-[--text-main] w-full" />
